5d94f800afcbc6de2727924aeff904a7219e5e8d
YUN EN TSAI <eyan505@gmail.com>
2024-12-06 20:18:41 -0500

    Feat: adjust page redirct to mutilapps and change layout

--- frontend.py
+++ frontend.py
@@ -1,68 +1,12 @@
 import streamlit as st
+from multiapps import MultiApp
+from pages import searchbar, recipe
 
-# set the page and title
-st.set_page_config(page_title="Chefpal.ai", layout="centered")
-hide_sidebar_style = """
-    <style>
-        [data-testid="stSidebar"] {
-            display: none;  /* 隱藏整個 Sidebar */
-        }
-    </style>
-"""
-st.markdown(hide_sidebar_style, unsafe_allow_html=True)
+# Set page config once here, at the start
+st.set_page_config(page_title="Chefpal.ai", layout="wide")
 
-# declare menu type
-cuisine_options = ["Chinese", "Thai", "Japanese", "American", "French", "Italy"]
+app = MultiApp()
+app.add_app("Searchbar", searchbar.app)
+app.add_app("Recipe", recipe.app)
 
-# initiate
-if "selected_cuisine" not in st.session_state:
-    st.session_state.selected_cuisine = None  
-
-# topic
-st.markdown(
-    """
-    <div style="text-align: center;">
-        <h1 style="color: #444;">Chefpal.ai</h1>
-        
-    </div>
-
-     <style>
-        .stApp {
-            background-color: #F5E3C2;
-        }
-     </style>
-    """,
-    unsafe_allow_html=True,
-)
-
-st.markdown("### What's in your fridge?")
-col1, col2 = st.columns([4, 1])  # adjust the ratio for searchbar and submit button
-with col1:
-    input_text = st.text_input("", placeholder="Message Chefpal", label_visibility="collapsed")
-with col2:
-    submit_button = st.button("➤")
-
-if submit_button:
-    st.write(f"You entered: {input_text}")
-    # Redirect to recipe.py
-    st.switch_page("pages/recipe.py")
-
-st.markdown("### Choose a cuisine:")
-
-# st.columns adjust horizontal
-columns = st.columns(len(cuisine_options))
-for idx, cuisine in enumerate(cuisine_options):
-    with columns[idx]:
-        # button action
-        if st.button(cuisine, key=cuisine, use_container_width=True):
-            if st.session_state.selected_cuisine == cuisine:
-                st.session_state.selected_cuisine = None
-            else:
-                st.session_state.selected_cuisine = cuisine
-
-# show the menu type selected
-st.markdown("### Current Selection:")
-if st.session_state.selected_cuisine:
-    st.write(f"Selected Cuisine: {st.session_state.selected_cuisine}")
-else:
-    st.write("No cuisine selected.")
+app.run()
